92d5a7c7c624b6fd9cb93de3f29f946b73601a50
Neeraj <neeraj7500728999@gmail.com>
2022-08-05 13:11:13 +0530

    second commit

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import './Projects.css'
 import img1 from './img1.jpeg'
+import img2 from './Traker.jpeg'
 import Carousel from 'react-elastic-carousel'
+import img3 from './Weather.jpeg'
+import img4 from './Music.jpeg'
 import Tilt from 'react-vanilla-tilt'
 
 export default function Projects() {
@@ -9,7 +12,7 @@ export default function Projects() {
     { width: 1, itemsToShow: 1 },
     { width: 550, itemsToShow: 1},
     { width: 968, itemsToShow: 2 },
-    { width: 1200, itemsToShow: 3 },
+    { width: 1500, itemsToShow: 3 },
   ];
   return (
     <div>
@@ -18,7 +21,7 @@ export default function Projects() {
           <p data-aos="fade-down" className='para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui est quos odio, laudantium esse nihil suscipit earum laboriosam et voluptas, tempore quisquam assumenda totam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Id porro illo ipsum sed officia, quia repudiandae animi fugit facilis autem atque, doloribus perspiciatis?</p>
         <div data-aos="fade-down" className="container">
          
-          <Carousel breakPoints={breakPoints} >
+          <Carousel className='carousel' breakPoints={breakPoints}  >
           <Tilt className='ala' style={{width:'471px',height:'531px',marginBottom:'38px'}}>
              <div className="Card">
               <img src={img1} alt=""/>
@@ -26,15 +29,29 @@ export default function Projects() {
               <p >It's News App that fetch the Latest News from the News API.We have different News Category as per User Interest can watch.</p>
               </div>
               </Tilt>
+              <Tilt className='ala' style={{width:'471px',height:'531px',marginBottom:'38px'}}>
+             <div className="Card">
+              <img src={img2} alt=""/>
+              <h3>Expense Tracker</h3>
+              <p >It's Expense Tracker App that can track your all daily Expenses and Income.</p>
+              </div>
+              </Tilt>
+              <Tilt className='ala' style={{width:'471px',height:'531px',marginBottom:'38px'}}>
+             <div className="Card">
+              <img src={img3} alt=""/>
+              <h3>Weather webApp</h3>
+              <p >It's a Weather App that shows Live Weather Condition of all countries.</p>
+              </div>
+              </Tilt>
+              <Tilt className='ala' style={{width:'471px',height:'531px',marginBottom:'38px'}}>
+             <div className="Card">
+              <img src={img4} alt=""/>
+              <h3>MusicPlayer WebApp</h3>
+              <p >It's MusicPlayer App that can Play the Music of your custom Music Gallery.</p>
+              </div>
+              </Tilt>
            
               
-              <div className="Card">Hello</div>
-              <div className="Card">Hello</div>
-              <div className="Card">Hello</div>
-              <div className="Card">Hello</div>
-              <div className="Card">Hello</div>
-              <div className="Card">Hello</div>
-               <div className="Card" >Hello</div>
           </Carousel>
          
         </div>
